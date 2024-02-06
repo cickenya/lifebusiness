6a4ede8f94a59a9b2d34c8a6092e8789ca60a704
corporate insurance <151666354+cickenya@users.noreply.github.com>
2024-02-06 22:29:18 +0300

    Update life.py

--- life.py
+++ life.py
@@ -148,11 +148,9 @@ if st.button("Calculate"):
     
     merged = pd.merge(table1, table, on='year', how='inner')
 
-    finalmerged = merged[['year', 'premium', 'commission']]
-
-    finalmerged = finalmerged.rename({'premium':'Premium Received', 'commission':'Commission Payable'})
-
+   finalmerged = merged[['year', 'premium', 'commission']].rename(columns={'year':'As at End of Year:', 'premium': 'Premium Received', 'commission': 'Commission Payable'})
     st.table(finalmerged)
+
         
 
 
