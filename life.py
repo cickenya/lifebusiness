ccd77e07d21f18f4b3d336037cb222ef8e332695
Collins Chetekei <112887160+chetekei@users.noreply.github.com>
2024-02-08 12:55:50 +0300

    Update life.py

--- life.py
+++ life.py
@@ -24,7 +24,7 @@ commission_rate_year_three_onwards = 0.05
 premium_month_one = (policies*amount*agents*unit_managers*persistency)
 comm_premium = 6*(premium_month_one + (premium_month_one + ((12-1) * premium_month_one)))
 
-st.write(Annual Premium For New Business: {comm_premium})
+st.write("Annual Premium For New Business:" ,comm_premium)
 
 
 if st.button("Calculate"):
