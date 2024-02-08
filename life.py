be9e871f63cd97d070ab0ad60adf278b08a1af84
Collins Chetekei <112887160+chetekei@users.noreply.github.com>
2024-02-08 12:36:08 +0300

    Update life.py

--- life.py
+++ life.py
@@ -23,7 +23,7 @@ commission_rate_year_three_onwards = 0.05
 
 premium_month_one = (policies*amount*agents*unit_managers*persistency)
 comm_premium = 6*(premium_month_one + (premium_month_one + ((12-1) * premium_month_one))
-
+st.write(comm_premium)
 
 if st.button("Calculate"):
     def calculate_premium(years):   
@@ -149,7 +149,7 @@ if st.button("Calculate"):
 
     finalmerged = merged[['year', 'premium', 'commission', 'Cumulative Premium']].rename(columns={'year':'As at End of Year:', 'premium': 'Premium Received', 'commission': 'Commission Payable'})
     st.table(finalmerged)
-    st.write(comm_premium)
+    
         
 
 
