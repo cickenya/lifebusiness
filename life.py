fd747ceff1b7981e6ad73d7a440994ce1f740b2f
Collins Chetekei <112887160+chetekei@users.noreply.github.com>
2024-02-08 12:37:01 +0300

    Update life.py

--- life.py
+++ life.py
@@ -23,7 +23,7 @@ commission_rate_year_three_onwards = 0.05
 
 premium_month_one = (policies*amount*agents*unit_managers*persistency)
 comm_premium = 6*(premium_month_one + (premium_month_one + ((12-1) * premium_month_one))
-st.write(comm_premium)
+st.markdown(comm_premium)
 
 if st.button("Calculate"):
     def calculate_premium(years):   
